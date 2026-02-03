a8bd9cb74adc93db79f40abe3aba2ccfc3a6d55b
ShilpaS2001 <sshilpa78912@gmail.com>
2026-02-03 18:43:17 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const app = require("./src/app");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error(err));
